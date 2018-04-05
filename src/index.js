06278ac9fff2560511c9a03a418b35cd046016e9
edwardZhangYU <996060068@qq.com>
2018-04-05 15:01:14 +0800

    s

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const counter = function(){};
+const store = createStore(counter, compose(
+   applyMiddleware(thunk)
+));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (<Provider store={{store}}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>), document.getElementById('root'));
 registerServiceWorker();
