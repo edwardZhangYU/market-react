e06803a45607e48aa682ed3754a9744b585e78b2
zhangyu <zhangyu@zhangyu.local>
2019-05-20 13:50:18 +0800

    记录

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { connect } from 'react-redux';
+import { addGun, asyncAddGun } from './index.redux'
+//@connect(statetoProps,actionCreator)
 class App extends Component {
   render() {
     return (
@@ -17,5 +19,14 @@ class App extends Component {
     );
   }
 }
-
+/* 
+*connect与Provide联合优化redux的使用
+*connect接受参数
+*一个statetoProps(state){return {xxx:state}}函数
+*一个包含action的对象{} ----actionCreator
+*至此action和state数据都挂在了组件的props里
+*并且调用action 时，不用store.dispatch。注入的action，eg：addGun自动带dispatch功能
+* */
+//App = connect(statetoProps,actionCreator)(App)
+//装饰器写法：见类上
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//applyMiddleware 管理中间件
 import { createStore, applyMiddleware, compose} from 'redux';
+//action里有异步操作
 import thunk from 'redux-thunk';
+
+//优雅的结合react，redux
+/* 提供Provider 和connect
+*   Provider 组件包裹App，使store自动注入各组件
+*   connect 负责从外部获取组件需要的参数，可用装饰器写法书写
+*/
 import { Provider } from 'react-redux';
+//BrowserRouter包裹应用 Link to跳转  Router path component路由占位
 import { BrowserRouter } from 'react-router-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-const counter = function(){};
-const store = createStore(counter, compose(
-   applyMiddleware(thunk)
+
+import { counter } from './index.redux'
+
+/*用reducer初始化store
+*store.getState         store.dispatch(action)      
+*/
+//devToolsExtension配合reudx的谷歌插件debug用
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f=>f
+//compose组合函数
+const store = createStore(counter,compose(
+    applyMiddleware(thunk),
+    reduxDevtools
 ));
 
+
 ReactDOM.render(
-    (<Provider store={{store}}>
+    (<Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
